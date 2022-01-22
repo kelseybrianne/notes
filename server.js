2b0313e6dc953a2bc5a1d9d8cdb241807779ffa8
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-21 20:20:01 -0800

    add comments to server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
-const { arrayBuffer } = require('stream/consumers');
 
 const app = express();
 const PORT = 3001;
@@ -18,20 +17,21 @@ app.get('/', (req, res) =>
 );
 
 app.get('/notes', (req, res) =>
+
     // Send the file notes.html
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
 
+    // Send the notes in the database to the notes url being requested
     res.json(db)
     console.info(`${req.method} request received to get note`);
 })
 
 app.post('/api/notes', (req, res) => {
-    // Create (persist) data
-
-    // Access the new note data from 'req
+   
+    // Access the new note data from req.body
     const {title, text} = req.body;
     let id = uuidv4()
 
@@ -42,12 +42,12 @@ app.post('/api/notes', (req, res) => {
             id 
         };
         
-        // Push it to my existing list of notes
+        // Push the new note to my existing list of notes in the database
         db.push(newNote);
 
         const noteString = JSON.stringify(db, null, 2);
         
-        // Write my updates note list to the 'db.json' file
+        // Write my updated note list to the 'db.json' file
         fs.writeFile(`./db/db.json`, noteString, (err) =>
         err
         ? console.error(err)
@@ -55,13 +55,11 @@ app.post('/api/notes', (req, res) => {
             `New note has been written to JSON file`
           )
         );
-
         const response = {
             status: 'success',
             body: newNote,
         };
-
-        console.log(response);
+  
         res.status(201).json(response); 
     } else {
         res.status(500).json('Error in posting note');
@@ -69,17 +67,15 @@ app.post('/api/notes', (req, res) => {
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-
-    res.send("inside delete request");
-
     if (req.params.id) {
         console.info(`${req.method} request received to delete a note`);
         const noteId = req.params.id;
         let idMatches = false;
+
+        // Find the note in the database that matches the parameter id and take it out of the database array
         for (let i = 0; i < db.length; i++) {
             const currentNote = db[i];
             if (currentNote.id === noteId) {
-                // res.status(200).json(currentNote);
                 db.splice(i, 1);
                 idMatches = true;
             }
@@ -87,7 +83,7 @@ app.delete('/api/notes/:id', (req, res) => {
         if (idMatches) {
             const noteString = JSON.stringify(db, null, 2);
         
-            // Write my updates note list to the 'db.json' file
+            // Write my updated note list to the 'db.json' file
             fs.writeFile(`./db/db.json`, noteString, (err) =>
             err
             ? console.error(err)
@@ -97,40 +93,13 @@ app.delete('/api/notes/:id', (req, res) => {
             ); 
             return;
         }
-        
-        // res.status(404).send('Note not found');
+        res.send('Note not found');
     } else {
-        // res.status(400).send('Note ID not provided');
-      }
-    // fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    //     if (err) {
-    //         console.error(err);
-    //       } else {
-    //         // Convert string into JSON object
-    //         const parsedDb = JSON.parse(data);
-
-    //         for(var i = 0; i < parsedDb.length; i++) {
-    //             if (parsedDb[i].id === idSelectedForDeleting) {
-    //                 parsedDb.splice(i, 1);
-    //             }
-    //         }
-    
-    //         // Add a new review
-    //         parsedReviews.push(newReview);
-    
-    //         // Write updated reviews back to the file
-    //         fs.writeFile(
-    //           './db/reviews.json',
-    //           JSON.stringify(parsedReviews, null, 4),
-    //           (writeErr) =>
-    //             writeErr
-    //               ? console.error(writeErr)
-    //               : console.info('Successfully updated reviews!')
-    //         );
-    //     }
-    // });
+        res.send('Note ID not provided');
+    }
 });
 
+// Listen from port 3001
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
 );
