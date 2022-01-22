2233325ee47ae6902dfa566658fe3ec3bceb65aa
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-21 17:05:05 -0800

    add a get * methodto return the index.html file

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(_dirname, 'public/index.html'))
+);
+
 app.get('/notes', (req, res) =>
     // Send the file notes.html
   res.sendFile(path.join(__dirname, 'public/notes.html'))
